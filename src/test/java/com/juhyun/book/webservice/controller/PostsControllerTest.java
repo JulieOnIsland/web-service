7ed289d808deb9ff18d2f4262c632b8753068764
Juhyun Lee <juhyun62015@gmail.com>
2024-02-22 00:31:26 +0900

    게시글 수정 API 테스트 코드 작성

--- src/test/java/com/juhyun/book/webservice/controller/PostsControllerTest.java
+++ src/test/java/com/juhyun/book/webservice/controller/PostsControllerTest.java
@@ -3,6 +3,7 @@ package com.juhyun.book.webservice.controller;
 import com.juhyun.book.webservice.domain.posts.Posts;
 import com.juhyun.book.webservice.domain.posts.PostsRepository;
 import com.juhyun.book.webservice.dto.PostsSaveRequestDto;
+import com.juhyun.book.webservice.dto.PostsUpdateRequestDto;
 import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -63,5 +66,39 @@ public class PostsControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void modifyPosts() {
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("test title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "steve jobs speech";
+        String expectedContent = "stay hungry, stay foolish";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+
+
     
 }
