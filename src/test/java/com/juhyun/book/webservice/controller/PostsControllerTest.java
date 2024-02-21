60d303cae1e276217e3b9e4351497d7d12359683
Juhyun Lee <juhyun62015@gmail.com>
2024-02-21 23:52:51 +0900

    게시글 저장하는 API 테스트 코드 작성

---
+++ src/test/java/com/juhyun/book/webservice/controller/PostsControllerTest.java
@@ -0,0 +1,67 @@
+package com.juhyun.book.webservice.controller;
+
+import com.juhyun.book.webservice.domain.posts.Posts;
+import com.juhyun.book.webservice.domain.posts.PostsRepository;
+import com.juhyun.book.webservice.dto.PostsSaveRequestDto;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsControllerTest {
+
+    // @WebMvcTest는 JPA 기능이 작동하지 않아 JPA 기능까지 한 번에 테스트할 때는
+    // @SpringBootTest와 TestRestTemplate을 사용하면 된다
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After("")
+    public void tearDown() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void registerPosts() {
+        String title = "test title";
+        String content = "test content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("juhyun")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+    
+}
